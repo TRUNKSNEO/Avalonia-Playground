cf2d2b779391f38f795db8e08a85d310c044f356
Júlio Campos <julioluciano7184@gmail.com>
2026-04-10 14:59:02 +0100

    weather ipma icons

--- MyAvalonia/Models/Weather/WeatherTypeDto.cs
+++ MyAvalonia/Models/Weather/WeatherTypeDto.cs
@@ -17,20 +17,7 @@ namespace MyAvalonia.Models.Weather
 
 		public bool IsValid => IdWeatherType >= 0;
 
-		public string WeatherIcon => IdWeatherType switch
-		{
-			1 => "\uE472",
-			2 or 3 => "\uE540",
-			4 or 5 => "\uE1AA",
-			6 => "\uE53C",
-			7 or 8 or 9 => "\uE1B4",
-			10 or 11 or 12 => "\uE1B4",
-			13 or 14 or 15 => "\uE3BB",
-			16 or 17 or 18 => "\uE1B8",
-			20 => "\uE53C",
-			21 => "\uE53C",
-			30 => "\uE1B2",
-			_ => "\uE3B7"
-		};
+		public string WeatherIconPath => $"avares://MyAvalonia/Assets/Images/Weather/w_ic_d_{IdWeatherType:D2}.svg";
+
 	}
 }
