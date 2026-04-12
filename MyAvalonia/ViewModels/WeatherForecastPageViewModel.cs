15aeacce8f0b46651bbe1838ac7803ac04e63c5b
Luciano Campos <51826416+jlKampos@users.noreply.github.com>
2026-04-12 14:15:16 +0100

    ProgressDialog

--- MyAvalonia/ViewModels/WeatherForecastPageViewModel.cs
+++ MyAvalonia/ViewModels/WeatherForecastPageViewModel.cs
@@ -8,6 +8,7 @@ using MyAvalonia.Models.Forecast;
 using MyAvalonia.Models.Locations;
 using MyAvalonia.Models.Weather;
 using MyAvalonia.Models.Wind;
+using MyAvalonia.Utils;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,241 +17,254 @@ using System.Threading.Tasks;
 
 namespace MyAvalonia.ViewModels
 {
-	public partial class WeatherForecastPageViewModel : PageViewModel
-	{
-		private readonly IMapper _mapper;
-		private readonly IIpmaService _apiClient;
+    public partial class WeatherForecastPageViewModel : PageViewModel
+    {
+        private readonly IMapper _mapper;
+        private readonly IIpmaService _apiClient;
+        public Window? OwnerWindow { get; set; }
 
-		[ObservableProperty]
-		private ForecastItemDto? _selectedTab;
+        [ObservableProperty]
+        private ForecastItemDto? _selectedTab;
 
-		[ObservableProperty]
-		private LocationDto _selectedLocation;
+        [ObservableProperty]
+        private LocationDto _selectedLocation;
 
-		[ObservableProperty]
-		private AwarnessItemDto _awarness;
+        [ObservableProperty]
+        private AwarnessItemDto _awarness;
 
-		public ObservableCollection<LocationDto> Locations { get; } = new();
+        private ProgressDialog _progressDialog;
 
-		public ObservableCollection<ForecastItemDto> Forecasts { get; } = new();
+        public ObservableCollection<LocationDto> Locations { get; } = new();
 
-		private List<WeatherTypeDto> WeatherTypes { get; set; } = new();
-		private List<WindSpeedDto> WindSpeeds { get; set; } = new();
+        public ObservableCollection<ForecastItemDto> Forecasts { get; } = new();
 
-		private List<AwarnessItemDto> AwarnessTypes { get; set; } = new();
+        private List<WeatherTypeDto> WeatherTypes { get; set; } = new();
+        private List<WindSpeedDto> WindSpeeds { get; set; } = new();
 
-		// =========================
-		// RUNTIME CONSTRUCTOR
-		// =========================
-		public WeatherForecastPageViewModel(IIpmaService apiClient, IMapper mapper)
-		{
-			PageName = ApplicationPageNames.WeatherForecast;
+        private List<AwarnessItemDto> AwarnessTypes { get; set; } = new();
 
-			_mapper = mapper;
-			_apiClient = apiClient;
+        // =========================
+        // RUNTIME CONSTRUCTOR
+        // =========================
+        public WeatherForecastPageViewModel(IIpmaService apiClient, IMapper mapper)
+        {
+            PageName = ApplicationPageNames.WeatherForecast;
+            _progressDialog = new ProgressDialog();
+            _mapper = mapper;
+            _apiClient = apiClient;
 
-			_ = LoadWindAsync();
-			_ = LoadLocationsAsync();
-			_ = LoadWeatherTypesAsync();
-			_ = LoadAwarnessAsync();
-		}
+            _ = LoadWindAsync();
+            _ = LoadLocationsAsync();
+            _ = LoadWeatherTypesAsync();
+            _ = LoadAwarnessAsync();
+        }
 
-		// =========================
-		// DESIGN MODE CONSTRUCTOR
-		// =========================
-		public WeatherForecastPageViewModel()
-		{
-			if (Design.IsDesignMode)
-			{
-				PageName = ApplicationPageNames.WeatherForecast;
+        // =========================
+        // DESIGN MODE CONSTRUCTOR
+        // =========================
+        public WeatherForecastPageViewModel()
+        {
+            if (Design.IsDesignMode)
+            {
+                PageName = ApplicationPageNames.WeatherForecast;
 
-				Locations.Clear();
-				Locations.Add(new LocationDto { GlobalIdLocal = 1, Name = "Braga" });
-				Locations.Add(new LocationDto { GlobalIdLocal = 2, Name = "Porto" });
+                Locations.Clear();
+                Locations.Add(new LocationDto { GlobalIdLocal = 1, Name = "Braga" });
+                Locations.Add(new LocationDto { GlobalIdLocal = 2, Name = "Porto" });
 
-				WeatherTypes = new List<WeatherTypeDto>
-				{
-					new WeatherTypeDto { IdWeatherType = 1, DescriptionPT = "Limpo"},
-					new WeatherTypeDto { IdWeatherType = 10, DescriptionPT = "Chuva" }
-				};
+                WeatherTypes = new List<WeatherTypeDto>
+                {
+                    new WeatherTypeDto { IdWeatherType = 1, DescriptionPT = "Limpo"},
+                    new WeatherTypeDto { IdWeatherType = 10, DescriptionPT = "Chuva" }
+                };
 
-				WindSpeeds = new List<WindSpeedDto>
-				{
-					new WindSpeedDto { ClassWindSpeed = "1", DescriptionPT = "NW" }
-				};
+                WindSpeeds = new List<WindSpeedDto>
+                {
+                    new WindSpeedDto { ClassWindSpeed = "1", DescriptionPT = "NW" }
+                };
 
-				Forecasts.Clear();
+                Forecasts.Clear();
 
-				Forecasts.Add(new ForecastItemDto
-				{
-					DisplayDate = "03/04/2014",
-					WeatherTypeId = 1,
-					WeatherInformation = WeatherTypes[0]
-				});
+                Forecasts.Add(new ForecastItemDto
+                {
+                    DisplayDate = "03/04/2014",
+                    WeatherTypeId = 1,
+                    WeatherInformation = WeatherTypes[0]
+                });
 
-				Forecasts.Add(new ForecastItemDto
-				{
-					DisplayDate = "04/04/2014",
-					WeatherTypeId = 10,
-					WeatherInformation = WeatherTypes[1]
-				});
+                Forecasts.Add(new ForecastItemDto
+                {
+                    DisplayDate = "04/04/2014",
+                    WeatherTypeId = 10,
+                    WeatherInformation = WeatherTypes[1]
+                });
 
-				SelectedLocation = Locations.First();
-				SelectedTab = Forecasts.First();
+                SelectedLocation = Locations.First();
+                SelectedTab = Forecasts.First();
 
-				return;
-			}
-		}
+                return;
+            }
+        }
 
-		// =========================
-		// EVENTS
-		// =========================
-		partial void OnSelectedLocationChanged(LocationDto value)
-		{
-			if (value != null && !Design.IsDesignMode)
-			{
-				_ = LoadForecastAsync(value.GlobalIdLocal);
-			}
-		}
+        // =========================
+        // EVENTS
+        // =========================
+        partial void OnSelectedLocationChanged(LocationDto value)
+        {
+            RunWithProgressSelectedLocationChanged(value);
+        }
 
-		#region API Methods
+        private async void RunWithProgressSelectedLocationChanged(LocationDto value)
+        {
+            var dlg = ProgressDialog.StartShowProgressDialog(OwnerWindow, "Loading", "Please wait...");
 
-		private async Task LoadAwarnessAsync()
-		{
-			try
-			{
-				AwarnessTypes.Clear();
+            if (value != null && !Design.IsDesignMode)
+            {
+                await LoadForecastAsync(value.GlobalIdLocal);
+            }
 
-				var response = await _apiClient.GetAwarnessAsync();
-
-				if (response != null)
-				{
-					var mapped = _mapper.Map<List<AwarnessItemDto>>(response);
+            ProgressDialog.CloseShowProgressDialog(dlg);
+        }
 
 
-					foreach (var item in mapped)
-					{
-						AwarnessTypes.Add(item);
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				App.Current.HandleException(ex);
-			}
-		}
+        #region API Methods
 
-		private async Task LoadWindAsync()
-		{
-			try
-			{
-				WindSpeeds.Clear();
+        private async Task LoadAwarnessAsync()
+        {
+            try
+            {
+                AwarnessTypes.Clear();
 
-				var response = await _apiClient.GetWindAsync();
-				if (response.Data != null)
-				{
-					var mapped = _mapper.Map<List<WindSpeedDto>>(response.Data);
-					foreach (var item in mapped)
-					{
-						WindSpeeds.Add(item);
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				App.Current.HandleException(ex);
-			}
-		}
+                var response = await _apiClient.GetAwarnessAsync();
 
-		private async Task LoadLocationsAsync()
-		{
-			try
-			{
-				Locations.Clear();
-
-				var response = await _apiClient.GetLocationsAsync();
-
-				if (response.Data != null)
-				{
-					var mapped = _mapper.Map<List<LocationDto>>(response.Data);
+                if (response != null)
+                {
+                    var mapped = _mapper.Map<List<AwarnessItemDto>>(response);
 
 
-					foreach (var item in mapped)
-					{
-						Locations.Add(item);
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				App.Current.HandleException(ex);
-			}
-		}
+                    foreach (var item in mapped)
+                    {
+                        AwarnessTypes.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Current.HandleException(ex);
+            }
+        }
 
-		private async Task LoadWeatherTypesAsync()
-		{
-			try
-			{
-				var response = await _apiClient.GetWeatherTypesAsync();
-				if (response.Data != null)
-				{
-					var mapped = _mapper.Map<List<WeatherTypeDto>>(response.Data);
-					WeatherTypes.Clear();
-					foreach (var item in mapped)
-					{
-						WeatherTypes.Add(item);
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				App.Current.HandleException(ex);
-			}
-		}
+        private async Task LoadWindAsync()
+        {
+            try
+            {
+                WindSpeeds.Clear();
 
-		private async Task LoadForecastAsync(int locationId)
-		{
-			try
-			{
-				Forecasts.Clear();
+                var response = await _apiClient.GetWindAsync();
+                if (response.Data != null)
+                {
+                    var mapped = _mapper.Map<List<WindSpeedDto>>(response.Data);
+                    foreach (var item in mapped)
+                    {
+                        WindSpeeds.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Current.HandleException(ex);
+            }
+        }
 
-				var response = await _apiClient.GetForecastByCityAsync(locationId);
+        private async Task LoadLocationsAsync()
+        {
+            try
+            {
+                Locations.Clear();
 
-				if (response.Data != null)
-				{
-					var mapped = _mapper.Map<List<ForecastItemDto>>(response.Data);
+                var response = await _apiClient.GetLocationsAsync();
 
-					foreach (var item in mapped)
-					{
-						item.WeatherInformation =
-							WeatherTypes.FirstOrDefault(x => x.IdWeatherType == item.WeatherTypeId);
+                if (response.Data != null)
+                {
+                    var mapped = _mapper.Map<List<LocationDto>>(response.Data);
 
-						item.WindInformation =
-							WindSpeeds.FirstOrDefault(x => x.ClassWindSpeedValue == item.WindSpeedClass);
 
-						var awarnessForDay = AwarnessTypes
-						   .Where(a =>
-							   item.Date >= a.StartTime &&
-							   item.Date <= a.EndTime &&
-							   SelectedLocation.IdAreaAviso == a.Area)
+                    foreach (var item in mapped)
+                    {
+                        Locations.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Current.HandleException(ex);
+            }
+        }
 
-						   .ToList();
+        private async Task LoadWeatherTypesAsync()
+        {
+            try
+            {
+                var response = await _apiClient.GetWeatherTypesAsync();
+                if (response.Data != null)
+                {
+                    var mapped = _mapper.Map<List<WeatherTypeDto>>(response.Data);
+                    WeatherTypes.Clear();
+                    foreach (var item in mapped)
+                    {
+                        WeatherTypes.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Current.HandleException(ex);
+            }
+        }
 
-						// se tiveres uma propriedade
-						item.AwarnessInformation = awarnessForDay;
+        private async Task LoadForecastAsync(int locationId)
+        {
+            try
+            {
+                Forecasts.Clear();
 
-						Forecasts.Add(item);
-					}
+                var response = await _apiClient.GetForecastByCityAsync(locationId);
 
-					SelectedTab = Forecasts.FirstOrDefault();
-				}
-			}
-			catch (Exception ex)
-			{
-				App.Current.HandleException(ex);
-			}
-		}
+                if (response.Data != null)
+                {
+                    var mapped = _mapper.Map<List<ForecastItemDto>>(response.Data);
 
-		#endregion
-	}
+                    foreach (var item in mapped)
+                    {
+                        item.WeatherInformation =
+                            WeatherTypes.FirstOrDefault(x => x.IdWeatherType == item.WeatherTypeId);
+
+                        item.WindInformation =
+                            WindSpeeds.FirstOrDefault(x => x.ClassWindSpeedValue == item.WindSpeedClass);
+
+                        var awarnessForDay = AwarnessTypes
+                           .Where(a =>
+                               item.Date >= a.StartTime &&
+                               item.Date <= a.EndTime &&
+                               SelectedLocation.IdAreaAviso == a.Area)
+
+                           .ToList();
+
+                        // se tiveres uma propriedade
+                        item.AwarnessInformation = awarnessForDay;
+
+                        Forecasts.Add(item);
+                    }
+
+                    SelectedTab = Forecasts.FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                App.Current.HandleException(ex);
+            }
+        }
+
+        #endregion
+    }
 }
--- MyAvalonia/Views/WeatherForecastPageView.axaml.cs
+++ MyAvalonia/Views/WeatherForecastPageView.axaml.cs
@@ -1,11 +1,28 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Microsoft.Extensions.DependencyInjection;
+using MyAvalonia.ViewModels;
+using System;
 
 namespace MyAvalonia.Views;
 
 public partial class WeatherForecastPageView : UserControl
 {
-	public WeatherForecastPageView()
-	{
-		InitializeComponent();
-	}
+    public WeatherForecastPageView()
+    {
+        InitializeComponent();
+
+        this.AttachedToVisualTree += OnAttached;
+    }
+
+    private void OnAttached(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        var vm = DataContext as WeatherForecastPageViewModel;
+
+        if (vm != null)
+        {
+            vm.OwnerWindow = this.FindAncestorOfType<Window>();
+        }
+    }
 }
